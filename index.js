5f4bdf32d497a88de3191c614e688219797270c2
yousra40 <yusraalbaghdady123456@gmail.com>
2024-05-18 04:08:31 +0300

    done

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  for (let startIndex = 0; startIndex < word.length / 2; startIndex++) {
+    const endIndex = word.length - 1 - startIndex;
+    if (word[startIndex] !== word[endIndex]) {
+      return false;
+    }
+  }
+
+  return true;
 }
-
-/* 
-  Add your pseudocode here
-*/
-
-/*
-  Add written explanation of your solution here
-*/
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
